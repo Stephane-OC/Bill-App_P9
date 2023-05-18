f758efe7b3c78427f6dd2a8bc44c071669216139
Stephane-OC <stephane.fons.pro@gmail.com>
2023-05-18 17:28:12 +0200

    NewBill Tests & Fixes

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -15,6 +15,29 @@ import "@testing-library/jest-dom";
 
 jest.mock("../app/store", () => mockStore);
 
+/*******************************************************************
+********** NewBill Form Submission for Employee User ***************
+*******************************************************************/
+
+/* This test case verifies that an employee user can successfully complete NewBill form.        **
+** Main goal is to ensure that when an employee user navigates to NewBill page, they can        **
+** find form, fill it out, and submit it without encountering any errors.                       **
+
+** Test begins by setting up a mock navigation function, 'onNavigate', and mock localStorage.   **
+** 'localStorage' is set to mimic an employee user being currently logged in.                   **
+** Then, NewBill page's HTML is generated and set as body of document.                          **
+
+** A new instance of 'NewBill' class is created, representing employee user's current session.  **
+** NewBill form is then retrieved from page by its test ID and its existence on page            **
+** is confirmed by asserting that it is truthy.                                                 **
+
+** Mock function, 'handleSubmit', is set up to simulate the action of form being submitted.     **
+** This function calls 'handleSubmit' method of 'newBillTest' instance.                         **
+
+** An event listener is added to the NewBill form to call 'handleSubmit' when form is submitted.**
+** Finally, form is programmatically submitted using 'fireEvent.submit', and it is checked      **
+** that 'handleSubmit' was indeed called, confirming that the form submission was successful.   */
+
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
     test("Then complete form for newBill", async () => {
@@ -50,6 +73,24 @@ describe("Given I am connected as an employee", () => {
       expect(handleSubmit).toHaveBeenCalled();
     });
   });
+
+  /*******************************************************************
+  ********** Verification of Proof Picture Bill Upload ***************
+  *******************************************************************/
+
+  /* This test case verifies ability of an employee to upload proof picture for new bill.         **
+
+  ** Test starts by spying on 'bills' method of mock store to monitor interactions.               **
+
+  ** Then, necessary mocks are set up for navigation, localStorage, and window.location.          **
+  ** After that, NewBill page's HTML is generated, and a new 'NewBill' instance is created.       **
+
+  ** A mock file is created to simulate a proof picture and is uploaded using 'userEvent.upload'. **
+  ** Success of the upload is confirmed by checking  'name' property of the first file in         **
+  ** 'billFile.files'.                                                                            **
+
+  ** Finally, the form submission is simulated, and its success is confirmed by verifying that    **
+  ** 'handleSubmit' function was called.                                                          */
 
   test("Then verify the proof picture bill", async () => {
     jest.spyOn(mockStore, "bills");
@@ -96,6 +137,26 @@ describe("Given I am connected as an employee", () => {
   });
 });
 
+/*******************************************************************
+********** Submission of Unsupported File Type for a Bill **********
+*******************************************************************/
+
+/* This test case verifies that application correctly handles submission of an unsupported file type as a           **
+** bill's proof picture.                                                                                            **
+
+** Goal is to ensure that when an employee user attempts to upload file of type that is not supported               **
+** (i.e., not a JPG, JPEG, or PNG image), upload is rejected, and an appropriate error message is displayed.        **
+
+** Test begins by setting up necessary mocks for navigation, localStorage, and window.location,                     **
+** and creating NewBill page's HTML.                                                                                **
+
+** New instance of 'NewBill' class is then created, representing employee user's current session.                   **
+** Mock file of an unsupported type is created and an attempt is made to upload it using 'userEvent.upload'.        **
+** 'handleChangeFile' function is called during upload attempt, and it is confirmed that it was called as expected. **
+
+** Finally, it is checked that correct validation message is displayed, confirming that application                  **
+** correctly rejected unsupported file.                                                                              */
+
 test("Then verify the submission of an unsupported file type", async () => {
   const onNavigate = (pathname) => {
     document.body.innerHTML = ROUTES({ pathname });
@@ -123,7 +184,9 @@ test("Then verify the submission of an unsupported file type", async () => {
     localStorage: window.localStorage,
   });
 
-  const file = new File(["unsupported"], "unsupported.txt", { type: "text/plain" });
+  const file = new File(["unsupported"], "unsupported.txt", {
+    type: "text/plain",
+  });
   const handleChangeFile = jest.fn((e) => newBillInit.handleChangeFile(e));
   const formNewBill = screen.getByTestId("form-new-bill");
   const billFile = screen.getByTestId("file");
@@ -132,5 +195,7 @@ test("Then verify the submission of an unsupported file type", async () => {
   userEvent.upload(billFile, file);
 
   expect(handleChangeFile).toBeCalled();
-  expect(billFile.validationMessage).toBe("Merci d'utiliser le bon format JPG, JPEG ou PNG");
+  expect(billFile.validationMessage).toBe(
+    "Merci d'utiliser le bon format JPG, JPEG ou PNG"
+  );
 });
